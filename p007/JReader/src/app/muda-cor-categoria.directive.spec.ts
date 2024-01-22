57afa8a37cbca64b375f7866b5b3d486de212a3b
AllanaCampos <69643805+AllanaCampos@users.noreply.github.com>
2024-01-22 15:31:57 -0300

    Delete p007 directory

--- p007/JReader/src/app/muda-cor-categoria.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MudaCorCategoriaDirective } from './muda-cor-categoria.directive';
-
-describe('MudaCorCategoriaDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MudaCorCategoriaDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- p007/JReader/src/app/muda-cor-veiculos.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MudaCorVeiculosDirective } from './muda-cor-veiculos.directive';
-
-describe('MudaCorVeiculosDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MudaCorVeiculosDirective();
-    expect(directive).toBeTruthy();
-  });
-});
